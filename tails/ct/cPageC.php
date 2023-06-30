f26d78a3935406e8991a39eb4e7495175cdd92a2
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-30 09:55:25 +0200

    make view parameter optional for controllers

--- tails/ct/cPageC.php
+++ tails/ct/cPageC.php
@@ -3,21 +3,21 @@
 class cPageC
 {
   protected array $request = [];
-  protected object $view;
+  protected object $view; // optional - in case you want to have a hook-specific views
   protected ?array $prefs = null;
 
   /**
    * Summary of __construct
    * @param mixed $request
-   * @param mixed $view
    * @param mixed $prefs
+   * @param mixed $view
    * ________________________________________________________________
    */
-  public function __construct(array $request, object $view, ?array $prefs = null)
+  public function __construct(array $request, ?array $prefs = null, ?object $view = null)
   {
     $this->request = $request;
-    $this->view = $view;
     $this->prefs = $prefs;
+    $this->view = $view;
   }
 }
 
